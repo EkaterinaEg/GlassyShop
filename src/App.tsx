affdee1d12e7e72f3e73a128a292e43608028b8e
Ekaterina Egorova <61324163+EkaterinaEg@users.noreply.github.com>
2024-07-19 20:53:43 -0400

    feat: restore i18n for 2 languages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import './index.scss';
-import { Button } from './components/atoms/Button/Button.tsx';
+import { Button } from './components/atoms/Button';
+
+import { useTranslation } from 'react-i18next';
+import './i18n';
 
 function App() {
-  return <Button type='primary'>test</Button>;
+  const { t, i18n } = useTranslation();
+  const languages = [
+    { code: 'en', name: 'English' },
+    { code: 'fr', name: 'French' },
+  ];
+  return (
+    <>
+      <Button type='primary'>test</Button>
+      <h1>{t('footerTitle')}</h1>
+      {languages.map((language) => (
+        <Button type='secondary' onClick={() => i18n.changeLanguage(language.code)} key={language.code}>
+          {language.name}
+        </Button>
+      ))}
+    </>
+  );
 }
 
 export default App;
