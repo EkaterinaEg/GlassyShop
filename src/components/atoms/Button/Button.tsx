f9ea6ebe7c145f28b36ca912b5b0f16cd637eafe
Anton Pugachev <anton.pugachev@bluescape.com>
2024-07-20 18:53:38 -0400

    fix: [components_buttons_1] fix

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -5,28 +5,23 @@ import React from 'react';
 import './Button.scss';
 
 export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
-  /** Type of the component **/
+  /** Type of the component */
   type: 'primary' | 'secondary' | 'tertiary';
-  /** state loading or not **/
+  /** state loading or not */
   loading?: boolean;
   disabled?: boolean;
-  /** Adornment is displayed at the end of the button **/
+  /** Adornment is displayed at the end of the button */
   adornment?: React.ReactNode;
   children: React.ReactNode;
   onClick?: () => void;
 }
 
-export const Button = ({ type, onClick, children, disabled, loading, adornment }: ButtonProps) => {
-  // let btnClass = 'btn';
-  // if (type) {
-  //   btnClass += ` btn-${type}`;
-  // }
-
+export const Button = ({ type, onClick, children, disabled, adornment }: ButtonProps) => {
   const buttonClass = classNames('btn', `btn_${type}`);
   return (
     <button className={buttonClass} onClick={onClick} disabled={disabled}>
       {children}
-      {loading && <span className='btn__adornment'>{adornment}</span>}
+      {adornment && <span className='btn__adornment'>{adornment}</span>}
     </button>
   );
 };
